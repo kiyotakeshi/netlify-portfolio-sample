2598f7cb5b0617524da1bd25ba91a5350567effa
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-06-13 08:54:21 +0900

    jsx 使用した記述の例を追加

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import styles from "./App.module.css";
+import styles from './App.module.css';
 
 const App = () => {
-  return (
-    <div className={styles.container}>
-      <p className={styles.message}>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+    const message = 'hello React';
+    const isHide = true;
+    const people = ['Yamada', 'Tanaka', 'Sato'];
+    return (
+        <div className={styles.container}>
+            <p className={styles.message}>
+                {isHide ? (
+                    <p>isHide is true. {message}</p>
+                ) : (
+                    <p>isHide is false.</p>
+                )}
+
+                {people.map((person) => {
+                    return <p key={person}>{person}</p>;
+                })}
+            </p>
+        </div>
+    );
 };
 
 export default App;
