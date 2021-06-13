c0c1fd66a72d268d8aad0a94695710eaa1708f22
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-06-13 09:04:03 +0900

    props を渡すサンプル

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import styles from './App.module.css';
+import Hello from './Hello';
 
 const App = () => {
     const message = 'hello React';
     const isHide = true;
     const people = ['Yamada', 'Tanaka', 'Sato'];
     return (
-        <div className={styles.container}>
-            <p className={styles.message}>
-                {isHide ? (
-                    <p>isHide is true. {message}</p>
-                ) : (
-                    <p>isHide is false.</p>
-                )}
+        <>
+            <div className={styles.container}>
+                <div className={styles.message}>
+                    {isHide ? (
+                        <p>isHide is true. {message}</p>
+                    ) : (
+                        <p>isHide is false.</p>
+                    )}
 
-                {people.map((person) => {
-                    return <p key={person}>{person}</p>;
-                })}
-            </p>
-        </div>
+                    {people.map((person) => {
+                        return <p key={person}>{person}</p>;
+                    })}
+
+                    <Hello name="props" />
+                </div>
+            </div>
+        </>
     );
 };
 
