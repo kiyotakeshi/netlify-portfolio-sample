d8b7932cefb5ae5115ffcfdff479305d1e0b5033
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-06-13 23:12:07 +0900

    固定のサンプルデータを props により渡して画面に表示

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import styles from './App.module.css';
-import Hello from './Hello';
+import * as styles from './App.module.css';
 import Layout from './Layout';
+import PostItem from './PostItem';
+import WorkItem from './WorkItem';
 
 // CSS Modules により hoge.module.css ファイルを JS から呼び出し、
 // 各クラスごとに動的に生成されたクラス名を付与してくれる
@@ -10,11 +11,25 @@ import Layout from './Layout';
 // console.log(styles);
 // {container: "App_container__1MQN3", message: "App_message__2vrzR"}
 
-const App = () => {
+const App = (props) => {
     return (
         <>
             <Layout>
-                <p>This is the children</p>
+                <h2 className={styles.heading}>About</h2>
+                <p className={styles.profile}>
+                    Gatsby.js がどのようなものか学習し始めたところです。
+                </p>
+                <h2 className={styles.heading}>Posts</h2>
+                {props.posts.map((post) => {
+                    return <PostItem post={post} key={post.title} />;
+                })}
+                <h2 className={styles.heading}>Works</h2>
+                <div className={styles.workItems}>
+                    {props.works.map((work) => {
+                        return <WorkItem work={work} key={work.name} />;
+                    })}
+                </div>
+                <div className={styles.workItems}></div>
             </Layout>
         </>
     );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 // ブラウザごとにもつユーザーエージェントスタイルシートという差分を埋めて、
 // 表示を統一するために追加する、 CSS リセット
-import 'minireset.css'
-import "./index.css";
-import App from "./App";
+import 'minireset.css';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// sample data
+const posts = [
+    {
+        title: 'はてなブログ(iyokan)',
+        date: '2021/06/13',
+        url: 'https://ponkan1219.hateblo.jp/',
+    },
+    {
+        title: 'Qiita(ponkan1219)',
+        date: '2021/06/14',
+        url: 'https://qiita.com/ponkan1219',
+    },
+    {
+        title: 'GitHub(iyokan)',
+        date: '2021/06/15',
+        url: 'https://github.com/kiyotakeshi',
+    },
+];
+
+const works = [
+    {
+        title: 'はてなブログ(iyokan)',
+        roles: ['Author', 'Spring', 'C#'],
+        imageUrl: '/work.png',
+    },
+    {
+        title: 'Qiita(ponkan1219)',
+        roles: ['Author', 'RaspberryPi'],
+        imageUrl: '/work.png',
+    },
+    {
+        title: 'GitHub(iyokan)',
+        roles: ['Author', 'Git'],
+        imageUrl: '/work.png',
+    },
+];
+
+ReactDOM.render(
+    <App posts={posts} works={works} />,
+    document.getElementById('root')
+);
